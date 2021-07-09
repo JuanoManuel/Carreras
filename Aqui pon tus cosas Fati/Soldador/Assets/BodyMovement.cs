c12f3b95ea89adb2af952a59f234650a4a01e23e
imff <fuentes.fatima.eff@gmail.com>
2021-07-08 20:28:49 -0500

    The soldador
    body and movement

---
+++ Aqui pon tus cosas Fati/Soldador/Assets/BodyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyMovement : MonoBehaviour
+{
+    float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(1 * speed, 0, 0);
+            //Mathf.Clamp(0, -20, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(-1 * speed, 0, 0);
+            //Mathf.Clamp(0, -20, 0);
+        }
+    }
+}
---
+++ Aqui pon tus cosas Fati/Soldador/Assets/ObjectCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollision : MonoBehaviour
+{
+
+    CharacterController characterController;
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.collider.name == "Cube")
+            Debug.Log("I hit the cube!!");
+    }
+}
