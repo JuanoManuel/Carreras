4454a53c191bd648e9ac866957dbda468f4b198f
imff <fuentes.fatima.eff@gmail.com>
2021-07-23 22:36:10 -0500

    Collision detection
    Collision between objects.
    Exception is thrown, probably we need a try-catch

---
+++ Aqui pon tus cosas Fati/Soldador/Assets/Collision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public static int collisions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisions = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TAGGG: " + other.gameObject.transform.parent.gameObject.name);
+        if (other.gameObject.transform.parent.gameObject.name == "Robot")
+            collisions++;
+
+        Debug.Log("Lap: " + collisions);
+
+    }
+}
---
+++ Aqui pon tus cosas Fati/Soldador/Assets/Lap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lap : MonoBehaviour
+{
+    //public static int collisions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //collisions = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        collisions++; 
+        Debug.Log("Lap: "+ collisions);
+        Debug.Log(other.name + " hit me!! >:p");
+    }*/
+
+    void OnCollisionEnter(UnityEngine.Collision c)
+    {
+       Debug.Log(" enter!!");
+    }
+}
+
